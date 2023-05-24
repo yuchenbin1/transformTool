61da432241c45fdd8509ab90cb251bcf2d4df41a
yuchenbin <yuchenbin1@163.com>
2023-05-24 15:32:07 +0800

    依赖包的问题

--- src/main/index.js
+++ src/main/index.js
@@ -23,7 +23,12 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
   })
 
   mainWindow.loadURL(winURL)
